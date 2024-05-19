d796c2231e79d6ddb97f038824547081f261a8d4
SaturneAtom <saturneatom@gmail.com>
2024-05-19 23:04:55 +0200

    Update application.py

--- application.py
+++ application.py
@@ -299,4 +299,4 @@ def stress(seconds):
     return redirect(url_for("info"))
 
 if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=80, debug=True)
+    application.run(host="0.0.0.0", port=8080, debug=True)
